345f564d32f3ce809b897aaba5175eaaa28e96bf
qazer2525 <1006005@mymail.sutd.edu.sg>
2023-08-02 23:54:48 +0800

    nearly finished lol

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -11,6 +11,28 @@ import HomePage from "../components/HomePage";
 import Wish01 from "../components/Wish/Wish";
 import { AnimatePresence } from "framer-motion";
 import Aqua from "../components/Aquarium/Aquarium_final";
+import Game from "../components/Game/App";
+import { store, updateFrame, birdjump, game, states, rungame } from "../components/Game/store/store";
+
+
+function onpress(evt) {
+
+    switch (game.currentstate) {
+    default:
+    case states.Splash:
+      rungame()
+      birdjump(store.bird)
+      break
+    case states.Game:
+      birdjump(store.bird)
+      break
+    case states.Score:
+      break
+  }
+
+}
+document.addEventListener('mousedown', onpress);
+
 
 export default (
     <Router basename={`/${process.env.PUBLIC_URL}`}>
@@ -18,7 +40,7 @@ export default (
       <Routes>
         <Route path="" element={<HomePage />} />
         <Route path="wish/:name" element={<Wish01 />} />
-        <Route path="fish" element={<Aqua />} />
+        <Route path="game" element={<Game store={store} updateFrame={updateFrame} game={game}/>} />
         
       </Routes>
       </AnimatePresence>
